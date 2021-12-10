c0d81975d6d43936fd4f0ccaf32c3fba1ce56e65
Hedrich <vlandemir2411@hotmail.com>
2021-12-10 12:12:29 -0500

    Actualizando3

---
+++ Timer/Timer.X/main.c
@@ -0,0 +1,18 @@
+
+
+#include <xc.h>
+#include <pic18f4550.h>
+#include "config.h"
+#define _XTAL_FREQ 8000000UL
+   
+
+void main(void) {
+    
+    while(1){
+    
+    
+    }
+    
+    
+    return;
+}
