0d718cbf4d46d80b763ad9cccdf6f01d7f65d346
Hedrich <vlandemir2411@hotmail.com>
2021-12-12 09:31:01 -0500

    actualizando7

---
+++ Control PID/horno_pid.X/main.c
@@ -0,0 +1,24 @@
+/*
+ * File:   main.c
+ * Author: Vland
+ *
+ * Created on 11 December 2021, 20:01
+ */
+
+
+#include <xc.h>
+#include "librerias.h"
+
+
+
+
+void main(void) {
+    
+    while(1){
+        
+        
+    
+    }
+    
+    return;
+}
